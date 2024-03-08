ad7fcb57b0598f5ee71f3bacfb06f7b8680b719e
Vikranth <vickyvikranth005@gmail.com>
2024-03-08 13:25:29 +0530

    init

---
+++ src/sketch_mar7a/sketch_mar7a.ino
@@ -0,0 +1,13 @@
+const int ledPin = 2;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
